067b52c0ebac5aefabb3145da7bfe3e16b3fee50
FridaysBot <fridaysbotroblox@gmail.com>
2021-07-24 14:07:24 -0400

    Commit on main

--- index.js
+++ index.js
@@ -90,4 +90,20 @@ client.on('ready', () => {
         .setMongoPath(process.env.MONGO_URI)
 })
 
+client.on('guildMemberAdd', (member, message) => {
+    
+    const welcomeChannelId = '730909385622290513'
+
+    const welcomeEmbed = new Discord.MessageEmbed()
+    .setTitle('New Member Joined!')
+    .setColor('GREEN')
+    .setThumbnail('https://cdn.discordapp.com/attachments/782634444028772354/868546582629482586/1200px-Emoji_u1f44b.png')
+    .setDescription(`Welcome, <@${member.id}> to ${message.guild}! Please enjoy your stay and remember to read our rules.`)
+    .setTimestamp()
+
+    const channel = member.guild.channels.cache.get(welcomeChannelId)
+    channel.send(welcomeEmbed)
+
+})
+
 client.login(process.env.TOKEN)
